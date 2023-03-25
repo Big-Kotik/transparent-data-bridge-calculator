ec17dba6f148de2e4f169ad1c554d875c8bea41b
Big Kotik <big.kotik.5@gmail.com>
2023-03-26 01:00:36 +0300

    small fixes

--- app/src/main/java/com/bigkotik/calculator/events/queuehandler/StartRecordingEvent.kt
+++ app/src/main/java/com/bigkotik/calculator/events/queuehandler/StartRecordingEvent.kt
@@ -1,8 +1,5 @@
 package com.bigkotik.calculator.events.queuehandler
 
-import android.net.Uri
-import android.util.Log
-import com.bigkotik.calculator.transport.FileSender
 import com.bigkotik.calculator.voice.VoiceState
 
 class StartRecordingEvent<T>(sequence: Array<T>, private val state: VoiceState) :
