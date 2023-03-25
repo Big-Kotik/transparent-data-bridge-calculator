c545ccb4540ab3ed6cf7a01ed204b20951c91749
Big Kotik <big.kotik.5@gmail.com>
2023-03-25 22:48:48 +0100

    add initial voice recording support

--- app/src/main/java/com/bigkotik/calculator/events/queuehandler/StopRecordingEvent.kt
+++ app/src/main/java/com/bigkotik/calculator/events/queuehandler/StopRecordingEvent.kt
@@ -2,9 +2,9 @@ package com.bigkotik.calculator.events.queuehandler
 
 import com.bigkotik.calculator.voice.VoiceState
 
-class EndRecordingEvent<T>(sequence: Array<T>, private val state: VoiceState) :
+class StopRecordingEvent<T>(sequence: Array<T>, private val state: VoiceState) :
     ButtonsSequenceEvent<T>(sequence) {
     override fun execute() {
-        TODO("Not yet implemented")
+        state.stopRecording()
     }
 }
--- app/src/main/java/com/bigkotik/calculator/voice/VoiceState.kt
+++ app/src/main/java/com/bigkotik/calculator/voice/VoiceState.kt
@@ -1,4 +1,41 @@
 package com.bigkotik.calculator.voice
 
-class VoiceState {
+
+import android.media.MediaRecorder
+import com.bigkotik.calculator.MainActivity
+import java.io.IOException
+
+class VoiceState(currentDir: String) {
+
+    private var output: String? =
+        "${currentDir}/voice${System.currentTimeMillis()}.3gp"
+    private var mediaRecorder: MediaRecorder? = MediaRecorder(
+    ).apply {
+        setAudioSource(MediaRecorder.AudioSource.MIC)
+        setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        setOutputFile(output)
+    }
+    private var state: Boolean = false
+
+    // TODO: pause of needed
+//    private var recordingStopped: Boolean = false
+
+    public fun startRecording() {
+        try {
+            mediaRecorder?.prepare()
+            mediaRecorder?.start()
+            state = true
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    public fun stopRecording() {
+        mediaRecorder?.stop()
+        mediaRecorder?.release()
+        state = false
+    }
 }
