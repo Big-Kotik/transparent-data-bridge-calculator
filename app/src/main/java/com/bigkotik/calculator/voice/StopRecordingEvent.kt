b74444928ee1682d124e3b4ba0e4c4639b62ca9b
Big Kotik <big.kotik.5@gmail.com>
2023-03-26 15:49:00 +0300

    add settings

--- app/src/main/java/com/bigkotik/calculator/voice/StopRecordingEvent.kt
+++ app/src/main/java/com/bigkotik/calculator/voice/StopRecordingEvent.kt
@@ -2,10 +2,14 @@ package com.bigkotik.calculator.voice
 
 import android.util.Log
 import com.bigkotik.calculator.events.queuehandler.TransportButtonSequenceEvent
+import com.bigkotik.calculator.transport.FileSender
 import java.util.*
 
-class StopRecordingEvent<T>(sequence: Array<T>, private val state: VoiceState) :
-    TransportButtonSequenceEvent<T>(sequence) {
+class StopRecordingEvent<T>(
+    sequence: Array<T>, private val state: VoiceState,
+    fileSender: FileSender
+) :
+    TransportButtonSequenceEvent<T>(sequence, fileSender) {
 
     override fun execute() {
         state.stopRecording {
